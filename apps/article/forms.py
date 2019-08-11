8aa6ad507ebb8d8f3b2f0f948534e12049019c79
liweitao <1135577502@qq.com>
2019-08-11 08:46:31 +0800

    格式化代码

--- apps/article/forms.py
+++ apps/article/forms.py
@@ -5,6 +5,7 @@ from article.models import Article
 
 class Article_form(forms.ModelForm):
     category = forms.IntegerField(required=True)
+
     class Meta:
         model = Article
-        fields = ['title','authors','content']
+        fields = ['title', 'authors', 'content']
